1dcc220e2354b2efc5acf35fa3c9e5808aa4228d
Steven Braham <steven@braham.biz>
2017-02-16 16:34:15 +0100

    padding

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -11,7 +11,7 @@
     <title>Pizza plaza</title>
 </head>
 <body>
-<nav class="navbar navbar-inverse navbar-static-top">
+<nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
             <a class="navbar-brand" href=".">Pizza plaza</a>
